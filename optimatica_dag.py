8502835279e4bf29522bbc864ca955a9d5984632
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-10-27 09:52:57 +0300

    ver.2

--- optimatica_dag.py
+++ optimatica_dag.py
@@ -2,19 +2,26 @@ import requests
 import pandas as pd
 import sqlalchemy as sa
 from urllib.parse import quote
-import time
 import datetime as dt
 
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.hooks.base import BaseHook
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+from airflow.contrib.operators.vertica_operator import VerticaOperator
 
 
+api_con = BaseHook.get_connection('optimatica')
+dwh_con = BaseHook.get_connection('vertica')
 
-
+ps = quote(dwh_con.password)
 engine = sa.create_engine(
-    f"""vertica+vertica_python://shveynikovab:{quote('s@vy7hSA')}@vs-da-vertica:5433/sttgaz"""
+    f'vertica+vertica_python://{dwh_con.login}:{ps}@{dwh_con.host}:{dwh_con.port}/sttgaz'
 )
 
-
-base_url ='https://gaz-marketing.ru/api/v2/'
+base_url =api_con.host
 
 
 data_fields = {
@@ -325,8 +332,8 @@ def get_token():
     url = base_url + 'auth'
 
     data = {
-        "User": "integrator",
-        "Password": "EYjAe9qmnKy7"
+        "User": api_con.login,
+        "Password": api_con.password
     }
 
     response = requests.post(url, verify=False, json=data)
@@ -361,8 +368,12 @@ def write_data(data, table, period_from, period_to):
         index=False,
     )
 
+#-------------- callable function -----------------
+def get_data(data_type, **context):
 
-def get_data(period_from, period_to, data_type):
+    ex_date = context['execution_date']
+    period_from = f'{ex_date.year}-01-01T00:00:00+03:00'
+    period_to = f'{ex_date.year}-12-31T00:00:00+03:00'
 
     url = base_url + 'business-objects/query'
 
@@ -403,14 +414,143 @@ def get_data(period_from, period_to, data_type):
 
 
 
+#-------------- DAG -----------------
+default_args = {
+    'owner': 'Швейников Андрей',
+    'email': ['shveynikovab@st.tech'],
+    'retries': 4,
+    'retry_delay': dt.timedelta(minutes=30),
+}
+with DAG(
+        'Optimatica',
+        default_args=default_args,
+        description='Получение данных из Optimatica.',
+        start_date=days_ago(1),
+        schedule_interval='@daily',
+        catchup=True,
+        max_active_runs=1
+) as dag:
 
-start_time = time.time()
+    start = DummyOperator(task_id='Начало')
 
-get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'YearPlan')
-# get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'QuarterPlan')
-# get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'MinimumBudget')
-# get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'YearPlanItem')
-# get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'QuarterPlanItem')
-# get_data("2021-01-01T00:00:00+03:00", "2021-12-31T00:00:00+03:00" , 'Placement')
+    with TaskGroup('Загрузка_данных_в_stage_слой') as data_to_stage:
 
-print('Вренмя выполнения', time.time() - start_time)
+        get_year_plans = PythonOperator(
+            task_id='Получение_данных_об_годовых_планах',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'YearPlan'}
+        )
+
+        get_quarter_plans = PythonOperator(
+            task_id='Получение_данных_об_квартальных_планах',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'QuarterPlan'}
+        )
+
+        get_minimum_budgets = PythonOperator(
+            task_id='Получение_данных_об_минимальных_бюджетах',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'MinimumBudget'}
+        )
+
+        get_year_plan_items = PythonOperator(
+            task_id='Получение_данных_об_элементах_годового_плана',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'YearPlanItem'}
+        )
+
+        get_quarter_plan_items = PythonOperator(
+            task_id='Получение_данных_об_элементах_квартального_плана',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'QuarterPlanItem'}
+        )
+
+        get_placements= PythonOperator(
+            task_id='Получение_данных_об_размещениях',
+            python_callable=get_data,
+            op_kwargs={'data_type': 'Placement'}
+        )
+
+        [get_year_plans, get_quarter_plans, get_minimum_budgets, get_year_plan_items, get_quarter_plan_items, get_placements]
+
+    with TaskGroup('Формирование_слоя_DDS') as data_to_dds:
+
+        pass
+
+        # aux_mdaudit_regions = VerticaOperator(
+        #     task_id='update_aux_mdaudit_regions',
+        #     vertica_conn_id='vertica',
+        #     sql='aux_mdaudit_region.sql',
+        # )
+
+        # tables = (
+        #     'aux_mdaudit_shops',
+        #     'aux_mdaudit_divisions',
+        #     'aux_mdaudit_templates',
+        #     'aux_mdaudit_resolvers',
+        # )
+
+        # parallel_tasks = []
+
+        # for table in tables:
+        #     parallel_tasks.append(
+        #         VerticaOperator(
+        #             task_id=f'update_{table}',
+        #             vertica_conn_id='vertica',
+        #             sql=f'{table}.sql',
+        #         )
+        #     )
+
+        # aux_mdaudit_checks = VerticaOperator(
+        #     task_id='update_aux_mdaudit_checks',
+        #     vertica_conn_id='vertica',
+        #     sql='aux_mdaudit_checks.sql',
+        # )
+
+        # aux_mdaudit_answers = VerticaOperator(
+        #     task_id='update_aux_mdaudit_answers',
+        #     vertica_conn_id='vertica',
+        #     sql='aux_mdaudit_answers.sql',
+        # )
+
+        # aux_mdaudit_regions >> parallel_tasks >> aux_mdaudit_checks >> aux_mdaudit_answers
+
+    with TaskGroup('Формирование_слоя_dm') as data_to_dm:
+
+        pass
+
+        # dm_mdaudit_detailed = VerticaOperator(
+        #     task_id='update_dm_mdaudit_detailed',
+        #     vertica_conn_id='vertica',
+        #     sql='dm_mdaudit_detailed.sql',
+        # )
+
+        # dm_mdaudit_answers = VerticaOperator(
+        #     task_id='update_dm_mdaudit_answers',
+        #     vertica_conn_id='vertica',
+        #     sql='dm_mdaudit_answers.sql',
+        # )
+
+        # [dm_mdaudit_detailed, dm_mdaudit_answers]
+    
+    with TaskGroup('Проверка_данных') as data_check:
+
+        pass
+         
+        # check_1 = VerticaOperator(
+        #     task_id='checking_for_duplicates',
+        #     vertica_conn_id='vertica',
+        #     sql='checking_for_duplicates.sql'
+        # )
+
+        # check_2 = VerticaOperator(
+        #     task_id='checking_for_accuracy_of_execution',
+        #     vertica_conn_id='vertica',
+        #     sql='checking_for_accuracy_of_execution.sql'
+        # )
+
+        # [check_1, check_2]
+
+    end = DummyOperator(task_id='Конец')
+
+    start >> data_to_stage >> data_to_dds >> data_to_dm >> data_check >> end
