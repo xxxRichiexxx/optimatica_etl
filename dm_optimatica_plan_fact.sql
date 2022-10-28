75fddd3b768e4220c916237e0d9d73be521a11d6
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-10-28 21:16:36 +0300

    ver.6

--- dm_optimatica_plan_fact.sql
+++ dm_optimatica_plan_fact.sql
@@ -1,14 +1,18 @@
 CREATE OR REPLACE VIEW sttgaz.dm_optimatica_plan_fact AS
 WITH sq AS(
 	SELECT i.*, p.dealer_id 
-	FROM sttgaz.aux_optimatica_year_plan_items AS i
-	JOIN sttgaz.aux_optimatica_year_plans AS p 
+	FROM sttgaz.aux_optimatica_year_plan_items 	AS i
+	JOIN sttgaz.aux_optimatica_year_plans 		AS p 
 		ON i.plan_id =p.id 
 )
-SELECT sq.*, p.site, p.description, p.publish_count, p.measure_unit, p.price
+SELECT
+	sq.*,
+	p.placement_id,
+	(p.price / (DATEDIFF(day, p.period_from, p.period_to) + 1)) * (DATEDIFF(day, sq.period_from, sq.period_to) + 1) AS price_p_month
 FROM sq
 LEFT JOIN sttgaz.aux_optimatica_placements AS p 
-	ON sq.dealer_id = p.dealer_id 
-	AND sq.media = p.media AND sq.model = p.model
-	AND sq.period_from >= p.period_from
-	AND sq.period_to <= p.period_to;
+	ON (sq.dealer_id = p.dealer_id 
+	AND sq.media = p.media
+	AND sq.model = p.model
+	AND sq.period_from::date >= p.period_from::date
+	AND sq.period_from::date < p.period_to::date);
