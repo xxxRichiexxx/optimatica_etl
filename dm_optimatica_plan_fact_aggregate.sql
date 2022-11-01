976e4e8bd66e5152d9007c488d9f272b25f53fb6
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-11-01 09:56:47 +0300

    ver.8

--- dm_optimatica_plan_fact_aggregate.sql
+++ dm_optimatica_plan_fact_aggregate.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW dm_optimatica_plan_fact_aggregate AS
 SELECT
 	item_id,
 	plan_id,
-	dealer_id,
+	dealer_name,
 	deleted,
 	frozen,
 	"month",
@@ -12,11 +12,13 @@ SELECT
 	model,
 	plan_price,
 	SUM(placement_price) AS fact_rpice
-FROM sttgaz.dm_optimatica_plan_fact
+FROM sttgaz.dm_optimatica_plan_fact AS pf
+LEFT JOIN sttgaz.aux_optimatica_dealers AS d
+	ON pf.dealer_id = d.id 
 GROUP BY
 	item_id,
 	plan_id,
-	dealer_id,
+	dealer_name,
 	deleted,
 	frozen,
 	"month",
