b5c24b4a034899f0bff14fe77d2e285d8a8033d8
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-12-13 11:53:20 +0300

    изменение названия таблиц в слое DDS

--- dds_optimatica_dealers.sql
+++ dds_optimatica_dealers.sql
@@ -1,9 +1,10 @@
 TRUNCATE TABLE sttgaz.dds_optimatica_dealers;
+
 INSERT INTO sttgaz.dds_optimatica_dealers
 ("dealer_name", "dealer_id", "dealer_city", "dealer_address")
 SELECT DISTINCT
-    "Dealer",
+	FIRST_VALUE("Dealer") OVER(PARTITION BY Dealer_Id ORDER BY "Year" DESC)             AS "Dealer",
     "Dealer_Id",
-    "DealerCity",
-    "DealerLegalAddress"
+    FIRST_VALUE("DealerCity") OVER(PARTITION BY Dealer_Id ORDER BY "Year" DESC)         AS "DealerCity",
+    FIRST_VALUE("DealerLegalAddress") OVER(PARTITION BY Dealer_Id ORDER BY "Year" DESC) AS "DealerLegalAddress"
 FROM sttgaz.stage_optimatica_YearPlan;
